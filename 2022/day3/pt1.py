b4d72a85645cc97d1d13826e376300c9cd182a5b
François Charlier <francois.charlier@redhat.com>
2022-12-07 10:12:32 +0100

    Day3 reformat & add missing def from ipython's history

--- 2022/day3/pt1.py
+++ 2022/day3/pt1.py
@@ -1,13 +1,23 @@
 # coding: utf-8
 with open("input_real") as fd:
     rucksacks = fd.readlines()
-    
+
+
+def ltrprio(ltr):
+    if ltr.islower():
+        return ord(ltr) - ord("a") + 1
+    else:
+        return ord(ltr) - ord("A") + 27
+
+
 remains = []
 for rucksack in rucksacks:
     rucksack = rucksack.strip()
     if not rucksack:
         next
-    c1, c2 = set(rucksack[0:int(len(rucksack)/2)]), set(rucksack[int(len(rucksack)/2):])
-    remains.append((c1&c2).pop())
-    
-sum([ltrprio(l) for l in remains])
+    c1, c2 = set(rucksack[0 : int(len(rucksack) / 2)]), set(
+        rucksack[int(len(rucksack) / 2) :]
+    )
+    remains.append((c1 & c2).pop())
+
+print(sum([ltrprio(l) for l in remains]))
--- 2022/day3/pt2.py
+++ 2022/day3/pt2.py
@@ -2,11 +2,19 @@
 remains = []
 with open("input_real") as fd:
     rucksacks = fd.readlines()
-    
+
+
+def ltrprio(ltr):
+    if ltr.islower():
+        return ord(ltr) - ord("a") + 1
+    else:
+        return ord(ltr) - ord("A") + 27
+
+
 for n in range(0, len(rucksacks) - 1, 3):
     rs1 = set(rucksacks[n].strip())
-    rs2 = set(rucksacks[n+1].strip())
-    rs3 = set(rucksacks[n+2].strip())
-    remains.append((rs1&rs2&rs3).pop())
-    
-sum([ltrprio(l) for l in remains])
+    rs2 = set(rucksacks[n + 1].strip())
+    rs3 = set(rucksacks[n + 2].strip())
+    remains.append((rs1 & rs2 & rs3).pop())
+
+print(sum([ltrprio(l) for l in remains]))
